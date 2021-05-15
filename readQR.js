668e430b33959960bfcc3f57afa3540a8f605068
YutaMatsumura1016 <yuta.train1016@gmail.com>
2021-05-15 19:19:36 +0900

    動かん

--- readQR.js
+++ readQR.js
@@ -20,12 +20,6 @@ navigator.mediaDevices
     })
 
 
-    const code = jsQR(imageData, width, height, options?);
-
-    if (code) {
-    console.log("Found QR code", code);
-    }
-
 
     const canvas = document.querySelector('#js-canvas')
     const ctx = canvas.getContext('2d')
